1215e4a1df471340203cb02fade17ca34af13294
Emadabdi <155989126+Emadabdi@users.noreply.github.com>
2026-02-15 12:06:48 +0100

    Initial Vite React project setup with ESLint configuration

---
+++ timeline-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import Timeline from "./components/Timeline";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Interactive Timeline</h1>
+      <Timeline />
+    </div>
+  );
+}
+
+export default App;
